7560ad2a57a1823ed868f32dacaffc3ed07da271
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2025-12-16 19:03:10 +0000

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",        // ✅ Use static export
-  trailingSlash: true,     // ✅ Ensures clean URLs with / (good for static hosting)
-  images: {
-    unoptimized: true,     // ✅ Disable Next image optimizer (not needed for static hosting)
-  },
-}
+  reactStrictMode: true,
+  swcMinify: true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
