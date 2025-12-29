f67063468802ce72a8138c2fa7cd175670cef46f
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2025-12-29 10:42:38 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   darkMode: 'class', // REQUIRED for dark mode toggle
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.css"
   ],
   theme: {
     extend: {},
