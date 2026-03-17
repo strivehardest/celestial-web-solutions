7679c6bb48135c3d21d5d8dc159910d3a07f2b1c
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2026-03-17 14:25:59 +0000

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -111,6 +111,20 @@ function MyApp({ Component, pageProps }) {
           `,
         }}
       />
+      {/* Microsoft Clarity */}
+      <Script
+        id="microsoft-clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vx7nzhjymk");
+          `,
+        }}
+      />
 
       <Navbar />
       <CustomCursor />
