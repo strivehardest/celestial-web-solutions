436b96968ef971033bf54995bb6d14d128eb4191
Waliu Ibrahimah Aforlabi <ibrahimah2011@hotmail.com>
2026-05-23 19:31:19 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  publicExcludes: [
+    '!png/**/*',
+    '!portfolio/**/*',
+    '!images/**/*',
+    '!fonts/**/*',
+    '!videos/**/*',
+  ],
+  buildExcludes: [
+    /png\/.*/,
+    /portfolio\/.*/,
+    /images\/.*/,
+    /fonts\/.*/,
+    /videos\/.*/,
+  ],
 });
 
 /** @type {import('next').NextConfig} */
@@ -70,17 +84,18 @@ const nextConfig = {
 
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.seeklogo.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'seeklogo.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'assets.paystack.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'www.ntc.edu', pathname: '/**' },
-      { protocol: 'https', hostname: 'admin.wac.co', pathname: '/**' },
-      { protocol: 'https', hostname: 'www.intellibright.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'cdn.jsdelivr.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'cdn.worldvectorlogo.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'img.youtube.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'upload.wikimedia.org', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.seeklogo.com',    pathname: '/**' },
+      { protocol: 'https', hostname: 'seeklogo.com',           pathname: '/**' },
+      { protocol: 'https', hostname: 'assets.paystack.com',    pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com',    pathname: '/**' },
+      { protocol: 'https', hostname: 'www.ntc.edu',            pathname: '/**' },
+      { protocol: 'https', hostname: 'admin.wac.co',           pathname: '/**' },
+      { protocol: 'https', hostname: 'www.intellibright.com',  pathname: '/**' },
+      { protocol: 'https', hostname: 'cdn.jsdelivr.net',       pathname: '/**' },
+      { protocol: 'https', hostname: 'cdn.worldvectorlogo.com',pathname: '/**' },
+      { protocol: 'https', hostname: 'img.youtube.com',        pathname: '/**' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org',   pathname: '/**' },
+      { protocol: 'https', hostname: 'flagcdn.com',            pathname: '/**' },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 160, 256, 384],
